f4e0cd49aeae121d8b4c5759c4e91e7727f5a5df
Hridayansh Gupta <hridayansh3@gmail.com>
2025-07-27 23:24:57 +0530

    Time: 1 ms (99.50%) | Memory: 44 MB (66.26%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,22 +1,15 @@
 class Solution {
     public int compress(char[] chars) {
-        int index = 0;  // Write index
-        int i = 0;      // Read index
-
+        int index = 0;  
+        int i = 0;      
         while (i < chars.length) {
             char currentChar = chars[i];
             int count = 0;
-
-            // Count duplicates
             while (i < chars.length && chars[i] == currentChar) {
                 i++;
                 count++;
-            }
-
-            // Write the character
-            chars[index++] = currentChar;
-
-            // Write the count if > 1
+            }  
+            chars[index++] = currentChar;    
             if (count > 1) {
                 String countStr = Integer.toString(count);
                 for (char c : countStr.toCharArray()) {
@@ -24,7 +17,6 @@ class Solution {
                 }
             }
         }
-
         return index;
     }
 }
