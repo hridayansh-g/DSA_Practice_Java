d84c852da9752d558c177d7812e8cd97db520984
Hridayansh Gupta <hridayansh3@gmail.com>
2025-08-12 14:12:12 +0530

    Time: 2 ms (99.85%) | Memory: 50.2 MB (67.97%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,6 +1,29 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int m = nums.length;
+        int p = 0, q = 0, c1 = 0, c2 = 0;
+
+        for (int v : nums) {
+            if (p == v) c1++;
+            else if (q == v) c2++;
+            else if (c1 == 0) { p = v; c1 = 1; }
+            else if (c2 == 0) { q = v; c2 = 1; }
+            else { c1--; c2--; }
+        }
+
+        c1 = 0; c2 = 0;
+        for (int v : nums) {
+            if (v == p) c1++;
+            else if (v == q) c2++;
+        }
+
+        List<Integer> ans = new ArrayList<>(2);
+        int thr = m / 3;
+        if (c1 > thr) ans.add(p);
+        if (c2 > thr && q != p) ans.add(q);
+        return ans;
+
+      /*  int m = nums.length;
         List<Integer> ans = new ArrayList<>();
         HashSet<Integer> seen = new HashSet<>();
 
@@ -15,6 +38,6 @@ class Solution {
             }
             if (cnt > m / 3) ans.add(v);
         }
-        return ans;
+        return ans; */
     }
 }
