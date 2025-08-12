55e47e5a5324ebc5ee97d0e03909cf9b97438035
Hridayansh Gupta <hridayansh3@gmail.com>
2025-08-12 13:50:50 +0530

    Time: 8 ms (85.67%) | Memory: 47.1 MB (9.72%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,7 +2,33 @@ import java.util.*;
 
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals == null || intervals.length == 0) return new int[0][2];
+         if (intervals == null || intervals.length == 0) return new int[0][2];
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        int k = 0; 
+        int curStart = intervals[0][0], curEnd = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+            int s = intervals[i][0], e = intervals[i][1];
+            if (s <= curEnd) {
+                if (e > curEnd) curEnd = e;
+            } else {
+                intervals[k][0] = curStart;
+                intervals[k][1] = curEnd;
+                k++;
+                curStart = s;
+                curEnd = e;
+            }
+        }
+
+        intervals[k][0] = curStart;
+        intervals[k][1] = curEnd;
+        k++;
+
+        return Arrays.copyOf(intervals, k);
+
+      /*  if (intervals == null || intervals.length == 0) return new int[0][2];
 
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
@@ -20,5 +46,6 @@ class Solution {
         }
         merged.add(cur);
         return merged.toArray(new int[merged.size()][]);
+        */
     }
 }
