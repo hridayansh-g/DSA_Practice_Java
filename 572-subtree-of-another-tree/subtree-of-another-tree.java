663fa739a578b6c1e997336a5a3db049523b801d
Hridayansh Gupta <hridayansh3@gmail.com>
2025-08-13 11:14:55 +0530

    Time: 7 ms (7.04%) | Memory: 44.2 MB (81.80%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -14,7 +14,57 @@
  * }
  */
 class Solution {
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        String sRoot = serialize(root, new StringBuilder()).toString();
+        String sSub  = serialize(subRoot, new StringBuilder()).toString();
+        return kmpContains(sRoot, sSub);
+    }
+
+   
+    private StringBuilder serialize(TreeNode node, StringBuilder sb) {
+        if (node == null) return sb.append("#,");   // # marks null
+        sb.append('(').append(node.val).append("),");
+        serialize(node.left, sb);
+        serialize(node.right, sb);
+        return sb;
+    }
+
+    
+    private boolean kmpContains(String text, String pat) {
+        if (pat.length() == 0) return true;
+        int[] lps = buildLps(pat);
+        int i = 0, j = 0;
+        while (i < text.length()) {
+            if (text.charAt(i) == pat.charAt(j)) {
+                i++; j++;
+                if (j == pat.length()) return true;
+            } else if (j > 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+
+    private int[] buildLps(String pat) {
+        int[] lps = new int[pat.length()];
+        int len = 0;
+        for (int i = 1; i < pat.length(); ) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                lps[i++] = ++len;
+            } else if (len > 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i++] = 0;
+            }
+        }
+        return lps;
+    }
+
+
+    // Approach 1
+  /*  public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if (subRoot == null) return true;   // empty subRoot -> always true
         if (root == null) return false;
 
@@ -27,4 +77,5 @@ class Solution {
         if (a.val != b.val) return false;
         return same(a.left, b.left) && same(a.right, b.right);
     }
+    */
 }
