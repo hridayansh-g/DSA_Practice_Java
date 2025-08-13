1a8fb02d4ec21666283cf8b2b11a436c665a111c
Hridayansh Gupta <hridayansh3@gmail.com>
2025-08-13 11:26:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (40.64%) - LeetSync

---
+++ 100-same-tree/same-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+         StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
+        serialize(p, s1);
+        serialize(q, s2);
+        return s1.toString().equals(s2.toString());
+    }
+
+    private void serialize(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append("#,");
+            return;
+        }
+        sb.append(node.val).append(",");
+        serialize(node.left, sb);
+        serialize(node.right, sb);
+    }
+}
