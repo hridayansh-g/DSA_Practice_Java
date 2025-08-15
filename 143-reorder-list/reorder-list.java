e23ec1414318ef511bb7935aca4da5153d272591
Hridayansh Gupta <hridayansh3@gmail.com>
2025-08-15 22:37:49 +0530

    Time: 1 ms (99.65%) | Memory: 48.7 MB (15.39%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -12,6 +12,32 @@ class Solution {
     public void reorderList(ListNode head) {
          if (head == null || head.next == null) return;
 
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next; fast = fast.next.next;
+        }
+
+        ListNode prev = null, cur = slow.next;
+        slow.next = null; // split
+        while (cur != null) {
+            ListNode nxt = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = nxt;
+        }
+
+        ListNode p1 = head, p2 = prev;
+        while (p2 != null) {
+            ListNode t1 = p1.next, t2 = p2.next;
+            p1.next = p2;
+            p2.next = t1;
+            p1 = t1;
+            p2 = t2;
+        }
+
+        // approach
+        /* if (head == null || head.next == null) return;
+
         ArrayList<ListNode> a = new ArrayList<>();
         for (ListNode p = head; p != null; p = p.next) a.add(p);
 
@@ -22,5 +48,6 @@ class Solution {
             a.get(j).next = a.get(i); j--;
         }
         a.get(i).next = null; 
+        */
     }
 }
