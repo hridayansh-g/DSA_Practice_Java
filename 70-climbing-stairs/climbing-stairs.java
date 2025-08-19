a2c89cd1236c683c569bb32572d8a03ee194a4d0
Hridayansh Gupta <hridayansh3@gmail.com>
2025-08-20 00:05:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (38.89%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,6 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
 
+        if (n <= 2) return n;
+        int a = 1, b = 2;
+        for (int i = 3; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+        // approach 2
+        /*
          if (n <= 2) return n;
         int[] dp = new int[n + 1];
         dp[0] = 1; dp[1] = 1;
@@ -8,6 +18,9 @@ class Solution {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
+    */
+
+
         // approach 1
         /*
         if (n == 0 || n == 1) return 1;
