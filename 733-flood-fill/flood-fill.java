51bfab9cb38b5f1773454813e28675c40404d6c7
Hridayansh Gupta <hridayansh3@gmail.com>
2025-08-19 23:43:43 +0530

    Time: 1 ms (44.24%) | Memory: 45.4 MB (10.68%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,5 +1,29 @@
+import java.util.*;
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int m = image.length, n = image[0].length;
+        int start = image[sr][sc];
+        if (start == color) return image;
+
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{sr, sc});
+        image[sr][sc] = color;
+
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+        while (!q.isEmpty()) {
+            int[] p = q.poll();
+            for (int[] d : dir) {
+                int nr = p[0] + d[0], nc = p[1] + d[1];
+                if (nr>=0 && nc>=0 && nr<m && nc<n && image[nr][nc]==start) {
+                    image[nr][nc] = color;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
+        }
+        return image;
+    }
+    // approach 1
+   /* public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int m = image.length, n = image[0].length;
         int start = image[sr][sc];
         if (start == color) return image; 
@@ -18,4 +42,5 @@ class Solution {
         dfs(img, r, c + 1, start, newColor, m, n);
         dfs(img, r, c - 1, start, newColor, m, n);
     }
+    */
 }
