d2a1ac0d624b23f0ebebb4c28d0c4a8097cdd151
Hridayansh Gupta <hridayansh3@gmail.com>
2025-08-22 18:41:24 +0530

    Time: 2 ms (72.47%) | Memory: 41.1 MB (98.61%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,6 +1,19 @@
 class Solution {
     public int hIndex(int[] citations) {
+        Arrays.sort(citations);
         int n = citations.length;
+        int h = 0;
+        for (int i = 0; i < n; i++) {
+            int count = n - i;
+            if (citations[i] >= count) {
+                h = count;
+                break;
+            }
+        }
+        return h;
+
+        // approach 1
+        /* int n = citations.length;
         int h = 0;
         for (int i = 1; i <= n; i++) {
             int count = 0;
@@ -12,5 +25,6 @@ class Solution {
                 h = i;
         }
         return h;
+        */
     }
 }
