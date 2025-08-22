0dfe54d195e4c3d8fc0212e8ab631326b62ec451
Hridayansh Gupta <hridayansh3@gmail.com>
2025-08-22 18:39:55 +0530

    Time: 27 ms (71.47%) | Memory: 92.2 MB (36.13%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -1,26 +1,64 @@
 class RandomizedSet {
+    Map<Integer, Integer> map;
+    List<Integer> list;
+    Random rand;
+
+    public RandomizedSet() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        rand = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val))
+            return false;
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val))
+            return false;
+        int idx = map.get(val);
+        int last = list.get(list.size() - 1);
+        list.set(idx, last);
+        map.put(last, idx);
+        list.remove(list.size() - 1);
+        map.remove(val);
+        return true;
+    }
+
+    public int getRandom() {
+        return list.get(rand.nextInt(list.size()));
+    }
+
+    //approach 1
+
+    /*
     List<Integer> list;
     Random ran;
-
+    
     public RandomizedSet() {
         list = new ArrayList<>();
         ran = new Random();
     }
-
+    
     public boolean insert(int val) {
         if (list.contains(val))
             return false;
         list.add(val);
         return true;
     }
-
+    
     public boolean remove(int val) {
         return list.remove((Integer) val);
     }
-
+    
     public int getRandom() {
         return list.get(ran.nextInt(list.size()));
     }
+    */
 }
 
 /**
