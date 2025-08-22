0a960a0bfd9a554ba90f8ec614d9fcde01ebb66e
Hridayansh Gupta <hridayansh3@gmail.com>
2025-08-22 12:26:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (80.01%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -4,19 +4,33 @@ class Solution {
         if (n == 0)
             return 0;
 
+        int k = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[k - 1]) {
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+
+        // approach 1
+        /*   int n = nums.length;
+        if (n == 0)
+            return 0;
+        
         int i = 1;
         while (i < n) {
             if (nums[i] == nums[i - 1]) {
-
+        
                 for (int j = i; j < n - 1; j++) {
                     nums[j] = nums[j + 1];
                 }
                 n--;
-
+        
             } else {
                 i++;
             }
         }
         return n;
+        */
     }
 }
