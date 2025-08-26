da01d19fce49aa8d5a768b79573449c99724d943
Hridayansh Gupta <hridayansh3@gmail.com>
2025-08-26 14:18:56 +0530

    Time: 1 ms (40.44%) | Memory: 41.8 MB (54.58%) - LeetSync

---
+++ 68-text-justification/text-justification.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> res = new ArrayList<>();
+        int n = words.length, i = 0;
+        while (i < n) {
+            int j = i, len = words[i].length();
+            while (j + 1 < n && len + 1 + words[j + 1].length() <= maxWidth) {
+                j++;
+                len += 1 + words[j].length();
+            }
+            boolean last = (j == n - 1);
+            if (last || i == j) {
+                StringBuilder sb = new StringBuilder(words[i]);
+                for (int k = i + 1; k <= j; k++)
+                    sb.append(' ').append(words[k]);
+                while (sb.length() < maxWidth)
+                    sb.append(' ');
+                res.add(sb.toString());
+            } else {
+                int gaps = j - i;
+                StringBuilder[] between = new StringBuilder[gaps];
+                for (int g = 0; g < gaps; g++)
+                    between[g] = new StringBuilder(" ");
+                int currLen = len;
+                int p = 0;
+                while (currLen < maxWidth) {
+                    between[p].append(' ');
+                    currLen++;
+                    p = (p + 1) % gaps;
+                }
+                StringBuilder line = new StringBuilder(maxWidth);
+                for (int k = 0; k < gaps; k++) {
+                    line.append(words[i + k]).append(between[k]);
+                }
+                line.append(words[j]);
+                res.add(line.toString());
+            }
+            i = j + 1;
+        }
+        return res;
+    }
+}
