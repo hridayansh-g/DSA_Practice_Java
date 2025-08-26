41a5e0cdca5b423753bea4ca2b6b80647cfa9359
Hridayansh Gupta <hridayansh3@gmail.com>
2025-08-26 13:59:12 +0530

    Time: 3 ms (79.91%) | Memory: 45.5 MB (91.22%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,9 +1,31 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        java.util.Arrays.fill(left, 1);
+        java.util.Arrays.fill(right, 1);
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1])
+                left[i] = left[i - 1] + 1;
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1])
+                right[i] = right[i + 1] + 1;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += Math.max(left[i], right[i]);
+        return sum;
+
+        // approach 1
+        /*    int n = ratings.length;
         int[] c = new int[n];
         java.util.Arrays.fill(c, 1);
-
+        
         boolean changed = true;
         while (changed) {
             changed = false;
@@ -18,10 +40,11 @@ class Solution {
                 }
             }
         }
-
+        
         int sum = 0;
         for (int v : c)
             sum += v;
         return sum;
+        */
     }
 }
