a981daa2f82476c56572ca0e9d9e74e18e5c7c04
Hridayansh Gupta <hridayansh3@gmail.com>
2025-09-01 15:53:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (24.22%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -7,8 +7,31 @@ class Solution {
 
     public void gameOfLife(int[][] board) {
         int m = board.length, n = board[0].length;
-        int[][] next = new int[m][n];
 
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                int live = 0;
+                for (int[] d : DIRS) {
+                    int nr = r + d[0], nc = c + d[1];
+                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && (board[nr][nc] & 1) == 1)
+                        live++;
+                }
+                int cur = board[r][c] & 1;
+                int next = (cur == 1) ? ((live == 2 || live == 3) ? 1 : 0) : (live == 3 ? 1 : 0);
+                board[r][c] |= (next << 1);
+            }
+        }
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++)
+                board[r][c] >>= 1;
+        }
+
+        // approach 1
+        /*
+        int m = board.length, n = board[0].length;
+        int[][] next = new int[m][n];
+        
         for (int r = 0; r < m; r++) {
             for (int c = 0; c < n; c++) {
                 int live = 0;
@@ -24,8 +47,9 @@ class Solution {
                 }
             }
         }
-
+        
         for (int i = 0; i < m; i++)
             System.arraycopy(next[i], 0, board[i], 0, n);
+            */
     }
 }
