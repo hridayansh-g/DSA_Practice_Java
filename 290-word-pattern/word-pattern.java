4b3ef002067799acc0d29f0332e2727ace728216
Hridayansh Gupta <hridayansh3@gmail.com>
2025-09-01 16:33:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (92.86%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,5 +1,30 @@
+import java.util.*;
+
 class Solution {
     public boolean wordPattern(String pattern, String s) {
+        String[] w = s.split(" ");
+        if (pattern.length() != w.length)
+            return false;
+
+        Map<Character, String> p2w = new HashMap<>();
+        Map<String, Character> w2p = new HashMap<>();
+
+        for (int i = 0; i < w.length; i++) {
+            char p = pattern.charAt(i);
+            String word = w[i];
+            String mappedWord = p2w.get(p);
+            Character mappedPat = w2p.get(word);
+
+            if (mappedWord == null && mappedPat == null) {
+                p2w.put(p, word);
+                w2p.put(word, p);
+            } else if (!word.equals(mappedWord) || (mappedPat == null || mappedPat != p)) {
+                return false;
+            }
+        }
+        return true;
+        // approach 1
+        /*
         String[] w = s.split(" ");
         if (pattern.length() != w.length)
             return false;
@@ -13,5 +38,6 @@ class Solution {
             }
         }
         return true;
+        */
     }
 }
