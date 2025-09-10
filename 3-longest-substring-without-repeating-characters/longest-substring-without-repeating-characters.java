ec850703b861b3323e25e57c93ea9442e38b8251
Hridayansh Gupta <hridayansh3@gmail.com>
2025-09-10 09:41:32 +0530

    Time: 6 ms (72.34%) | Memory: 44.7 MB (70.80%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,31 +1,55 @@
+import java.util.*;
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int[] last = new int[256];
-        java.util.Arrays.fill(last, -1);
-        int left = 0, best = 0;
-
-        for (int r = 0; r < s.length(); r++) {
-            char c = s.charAt(r);
-            if (last[c] >= left)
-                left = last[c] + 1;
-            last[c] = r;
-            best = Math.max(best, r - left + 1);
+        if(s.length()==0){
+            return 0;
         }
-        return best;
+        if(s.length()==1){
+            return 1;
+        }
+        HashSet<Character> h= new HashSet<>();
+        int maxLength=0;
+        int left=0;
+        for(int right=0; right < s.length(); right++){
+            if(h.contains(s.charAt(right))){
+                h.remove(s.charAt(left));
+                left++;
+                right--;
+            }
+            else{
+                h.add(s.charAt(right));
+            }
+            maxLength= Math.max(maxLength, right-left+1);
+        }
+        return maxLength;
 
-        // approach 1
-        /*    int n = s.length(), best = 0;
-        for (int i = 0; i < n; i++) {
-            boolean[] seen = new boolean[256];
-            for (int j = i; j < n; j++) {
-                char c = s.charAt(j);
-                if (seen[c])
-                    break;
-                seen[c] = true;
-                best = Math.max(best, j - i + 1);
+    }
+    // approach 1
+    /*
+    private boolean checkDuplicate(String s){
+        for(int i=0; i<s.length()-1; i++){
+            for(int j=i+1; j<s.length(); j++){
+                if(s.charAt(i)== s.charAt(j)){
+                    return false;
+                }
             }
         }
-        return best;
-        */
+        return true;
     }
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length()==1){
+            return 1;
+        }
+        int maxLength= 0;
+        for(int i=0; i<s.length();i++){
+            for(int j=i+1; j<=s.length();j++){
+                String st= s.substring(i,j);
+                if(checkDuplicate(st)){
+                    maxLength= Math.max(st.length(), maxLength);
+                }
+            }
+        }
+        return maxLength;
+    }
+    */
 }
