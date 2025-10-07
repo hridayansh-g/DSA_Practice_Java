dad1186546ecc5747ed7e331972fd0c82c84f769
Hridayansh Gupta <hridayansh3@gmail.com>
2025-10-07 14:30:31 +0530

    Time: 14 ms (24.65%) | Memory: 49.3 MB (93.75%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,45 +1,21 @@
 import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> h1= new HashMap<>();
-        int n= nums.length;
-        for(int i=0;i<nums.length;i++){
-            if(h1.containsKey(nums[i])){
-                h1.put(nums[i], h1.get(nums[i])+1);
-                //h1 value ++
+        HashMap<Integer, Integer> hp = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(hp.containsKey(nums[i])){
+                hp.put(nums[i],(hp.get(nums[i]))+1);
             }
             else{
-                h1.put(nums[i],1);
+                hp.put(nums[i],1);
             }
-            if(h1.get(nums[i])>(n/2)){
+        }
+        for(int i=0; i<nums.length; i++){
+            if((hp.get(nums[i])) > nums.length/2){
                 return nums[i];
             }
         }
         return -1;
-        /*Arrays.sort(nums);
-        int n= nums.length;
-        return nums[n/2];
-*/
 
-        /*int count=0;
-        int maxMaj=0;
-        int temp;
-        int x=-1;
-        int xj=0;
-        for(int i=0;i<nums.length-1;i++){
-            temp=nums[i];
-            for(int j=i+1;j<nums.length;j++){
-                if(temp== nums[j]){
-                    count++;
-                    x=j;
-                }
-            }
-            if(count>maxMaj){
-                maxMaj=count;
-                xj=x;
-            }
-            count=0;
-        }
-        return nums[xj]; */
     }
 }
