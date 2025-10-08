ee42619e528b14b0eb6bb2b6007835214db02ba5
Hridayansh Gupta <hridayansh3@gmail.com>
2025-10-08 20:09:42 +0530

    Time: 23 ms (9.12%) | Memory: 49 MB (12.22%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,62 +1,25 @@
 import java.util.*;
-
 class Solution {
-    
-
-    
     public List<List<String>> groupAnagrams(String[] strs) {
-         HashMap<String, List<String>> map = new HashMap();
-
-        for(String s : strs) {
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String key = new String(arr);
-
-            if(!map.containsKey(key)) {
-                map.put(key, new ArrayList<>());
+        HashMap<String, List<String>> hp= new HashMap<>();
+        for(String s: strs){
+            int[] arr= new int[26];
+            for(int i=0; i<s.length(); i++){
+                char c= s.charAt(i);
+                arr[c-'a']++;
             }
-
-            map.get(key).add(s);
-        }
-
-        return new ArrayList<>(map.values());
-    // approach 1
-    /*
-        int n = strs.length;
-        boolean[] used = new boolean[n];
-        List<List<String>> res = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            if (used[i])
-                continue;
-            List<String> group = new ArrayList<>();
-            group.add(strs[i]);
-            used[i] = true;
-
-            for (int j = i + 1; j < n; j++) {
-                if (!used[j] && isAnagram(strs[i], strs[j])) {
-                    group.add(strs[j]);
-                    used[j] = true;
-                }
+            StringBuilder sb= new StringBuilder();
+            for(int x: arr){
+                sb.append('#');
+                sb.append(x+"");
             }
-            res.add(group);
+            String key= sb.toString();
+            if(!hp.containsKey(key)){
+                hp.put(key, new ArrayList<String>());
+            }
+            hp.get(key).add(s);
+            
         }
-        return res;
-        */
+        return new ArrayList<>(hp.values());
     }
-
-
-/*
-    private boolean isAnagram(String a, String b) {
-        if (a.length() != b.length())
-            return false;
-        int[] cnt = new int[26];
-        for (int i = 0; i < a.length(); i++)
-            cnt[a.charAt(i) - 'a']++;
-        for (int i = 0; i < b.length(); i++)
-            if (--cnt[b.charAt(i) - 'a'] < 0)
-                return false;
-        return true;
-    }
-    */
 }
